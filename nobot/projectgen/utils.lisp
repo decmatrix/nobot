65c4ac704f0f26aeae2e9226b44bc8562c60a721
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2021-02-21 22:15:23 +0200

    add project generation utils

---
+++ nobot/projectgen/utils.lisp
@@ -0,0 +1,16 @@
+;;;; Copyright (c) 2021 Bohdan Sokolovskyi
+;;;; Author: Bohdan Sokolovskyi <sokol.chemist@gmail.com>
+
+
+(uiop:define-package :nobot/projectgen/utils
+    (:use :cl)
+  (:import-from :alexandria
+                #:plist-hash-table)
+  (:import-from :yason
+                #:encode)
+  (:exports #:make-path))
+
+(defun make-path (path)
+  (merge-pathnames
+   *default-pathname-defaults*
+   path))
